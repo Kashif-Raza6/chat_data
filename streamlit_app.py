b9f737e2e2761c66a9740ea99f09030e59455961
Kashif-Raza6 <96944736+Kashif-Raza6@users.noreply.github.com>
2023-08-04 19:17:58 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,24 +18,24 @@ def main():
     # Get user input
     question = st.text_input('Enter your query:', '')
 
-        # Load the model and data
-        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-        data = UnstructuredCSVLoader('final.csv')
-        index = VectorstoreIndexCreator().from_loaders([data])
-        st.title("LangChain Chat with your Data App")
-        st.write("Enter your query in the text box below and press Enter.")
+    # Load the model and data
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+    data = UnstructuredCSVLoader('final.csv')
+    index = VectorstoreIndexCreator().from_loaders([data])
+    st.title("LangChain Chat with your Data App")
+    st.write("Enter your query in the text box below and press Enter.")
 
-        # Get user input
-        question = st.text_input('Enter your query:', '')
+    # Get user input
+    question = st.text_input('Enter your query:', '')
 
-        if st.button("Submit"):
-            if question.strip() != '':
-                # Get the response from the LangChain model
-                response = index.query(llm=llm, question=question, chain_type="stuff")
+    if st.button("Submit"):
+        if question.strip() != '':
+            # Get the response from the LangChain model
+            response = index.query(llm=llm, question=question, chain_type="stuff")
 
-                # Display the response
-                st.markdown("### Response:")
-                st.write(response)
+            # Display the response
+            st.markdown("### Response:")
+            st.write(response)
 
 if __name__ == "__main__":
     main()
