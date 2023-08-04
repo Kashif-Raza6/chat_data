43d2b5f0841f42528b5d5ad39b7eb74f5a991871
Kashif-Raza6 <96944736+Kashif-Raza6@users.noreply.github.com>
2023-08-04 17:18:35 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,21 @@ from langchain.indexes import VectorstoreIndexCreator
 from dotenv import load_dotenv
 
 # Load environment variables
-load_dotenv()
+st.sidebar.markdown("### OpenAI API Key")
+openai_api_key = st.sidebar.text_input("Enter your OpenAI API key:", type="password")
+
+# Check if the API key starts with "sk-"
+if openai_api_key.strip().startswith("sk-"):
+    OPENAI_API_KEY = openai_api_key.strip()
+    # Configure OpenAI API
+    openai.api_key = OPENAI_API_KEY
+    proceed = True
+else:
+    proceed = False
+    st.sidebar.warning("OpenAI API key should start with 'sk-'")
+
+OPENAI_API_KEY = openai_api_key
+
 
 # Load the model and data
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
