e18353ae6fd2ecdec4b8b71d5e20730fe42eabfa
Kashif-Raza6 <96944736+Kashif-Raza6@users.noreply.github.com>
2023-08-04 18:43:45 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,64 @@
-# streamlit_app.py
-
-import openai
-import streamlit as st
 from langchain.document_loaders import UnstructuredCSVLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.indexes import VectorstoreIndexCreator
 import os
 
+# Load environment variables
+st.sidebar.markdown("### OpenAI API Key")
+openai_api_key = st.sidebar.text_input("Enter your OpenAI API key:", type="password")
+
+# Check if the API key starts with "sk-"
+if openai_api_key.strip().startswith("sk-"):
+    OPENAI_API_KEY = openai_api_key.strip()
+    # Configure OpenAI API
+    openai.api_key = OPENAI_API_KEY
+    proceed = True
+else:
+    proceed = False
+    st.sidebar.warning("OpenAI API key should start with 'sk-'")
+
+OPENAI_API_KEY = openai_api_key
 
 
-
+# Load the model and data
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+data = UnstructuredCSVLoader('final.csv')
+index = VectorstoreIndexCreator().from_loaders([data])
 
 
 def main():
+    st.title("Sleek LangChain Chat App")
+    st.write("Enter your query in the text box below and press Enter.")
+
+    # Get user input
+    question = st.text_input('Enter your query:', '')
+
+    if st.button("Submit"):
+        if question.strip() != '':
+            # Get the response from the LangChain model
+            response = index.query(llm=llm, question=question, chain_type="stuff")
+
+            # Display the response
+            st.markdown("### Response:")
+            st.write(response)
     openai_api_key = st.sidebar.text_input("Enter your OpenAI API Key:", type="password")
     if openai_api_key !=None:
-        os.environ['OPENAI_API_KEY']= openai_api_key
+        os.enviorn['OPENAI_API_KEY']= openai_api_key
         # Load the model and data
         llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
         data = UnstructuredCSVLoader('final.csv')
         index = VectorstoreIndexCreator().from_loaders([data])
         st.title("LangChain Chat with your Data App")
         st.write("Enter your query in the text box below and press Enter.")
-    
+
         # Get user input
         question = st.text_input('Enter your query:', '')
-    
+
         if st.button("Submit"):
             if question.strip() != '':
                 # Get the response from the LangChain model
                 response = index.query(llm=llm, question=question, chain_type="stuff")
-    
+
                 # Display the response
                 st.markdown("### Response:")
                 st.write(response)
